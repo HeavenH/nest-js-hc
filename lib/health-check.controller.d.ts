a94b219c6759f4c61863b3df847441a5c87f1aa3
Gilmar Silva <gilmarheavensilva@hotmail.com>
2022-04-02 17:29:41 -0300

    chore: remove dist and package-lock

--- lib/health-check.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
-export declare class HealthController {
-    private health;
-    private http;
-    private readonly KEY_NAME;
-    private readonly ENDPOINT;
-    constructor(health: HealthCheckService, http: HttpHealthIndicator, KEY_NAME: string, ENDPOINT: string);
-    check(): Promise<import("@nestjs/terminus").HealthCheckResult>;
-}
--- lib/health-check.module.d.ts
+++
@@ -1,4 +0,0 @@
-import { DynamicModule } from '@nestjs/common';
-export declare class HealthCheckModule {
-    static forRoot(keyName: string, endpoint: string): DynamicModule;
-}
